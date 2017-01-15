184253c49c58cb4196d0f6a77a3beed1d2b75e80
Adam Wonak <adam.wonak@gmail.com>
2017-01-15 16:02:31 -0600

    improved library with config vars. Added tests.

--- dice/dice.go
+++ dice/dice.go
@@ -1,6 +1,7 @@
 package dice
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"regexp"
@@ -8,16 +9,39 @@ import (
 	"time"
 )
 
+var (
+	// Dice config
+	MIN_DICE  = 1
+	MAX_DICE  = 6
+	MIN_SIDES = 3
+	MAX_SIDES = 100
+
+	// Error types
+	ErrInvalidPattern = errors.New("Invalid pattern")
+	ErrInvalidNumDice = errors.New(fmt.Sprintf("Number of dice must be between %d and %d", MIN_DICE, MAX_DICE))
+	ErrInvalidSides   = errors.New(fmt.Sprintf("Dice sides must be between %d and %d", MIN_SIDES, MAX_SIDES))
+)
+
 type DiceRoll struct {
 	NumDice int
 	Sides   int
-	roll    string
+	seed    int64
 }
 
-func NewDiceRoll(roll string) (*DiceRoll, error) {
-	re := regexp.MustCompile("(\\d+)d(\\d+)")
-	result := re.FindStringSubmatch(roll)
+func NewDiceRoll(pattern string) (*DiceRoll, error) {
+	return NewDiceRollWithSeed(pattern, time.Now().UnixNano())
+}
 
+func NewDiceRollWithSeed(pattern string, seed int64) (*DiceRoll, error) {
+	// Parse Pattern regex
+	re := regexp.MustCompile("^(\\d+)d(\\d+)$")
+	result := re.FindStringSubmatch(pattern)
+
+	if len(result) == 0 {
+		return nil, ErrInvalidPattern
+	}
+
+	// Parse the number strings to ints
 	numDice, err := strconv.Atoi(result[1])
 	if err != nil {
 		return nil, err
@@ -28,22 +52,29 @@ func NewDiceRoll(roll string) (*DiceRoll, error) {
 		return nil, err
 	}
 
-	return &DiceRoll{numDice, sides, roll}, nil
+	// Validation rules
+	if numDice < MIN_DICE || numDice > MAX_DICE {
+		return nil, ErrInvalidNumDice
+	}
+	if sides < MIN_SIDES || sides > MAX_SIDES {
+		return nil, ErrInvalidSides
+	}
+
+	return &DiceRoll{numDice, sides, seed}, nil
 }
 
 func (d DiceRoll) String() string {
-	return fmt.Sprintf("%v", d.roll)
+	return fmt.Sprintf("%vd%v", d.NumDice, d.Sides)
 }
 
-func (d DiceRoll) Roll() int{
+func (d DiceRoll) Roll() int {
 	results := 0
 
 	for i := 0; i < d.NumDice; i++ {
-		rand.Seed(time.Now().UnixNano())
+		rand.Seed(d.seed)
 		v := rand.Intn(d.Sides) + 1
 		results += v
 	}
 
 	return results
 }
-
--- dice/dice_test.go
+++ dice/dice_test.go
@@ -1,1 +1,69 @@
 package dice
+
+import (
+	"testing"
+)
+
+var dicePatternTests = []struct {
+	pattern  string
+	numDice  int
+	sides    int
+	expected int
+}{
+	{"1d6", 1, 6, 1},
+	{"2d20", 2, 20, 30},
+	{"4d10", 4, 10, 20},
+}
+
+func TestValidDiceRollPatterns(t *testing.T) {
+
+	for _, tt := range dicePatternTests {
+
+		d, _ := NewDiceRollWithSeed(tt.pattern, 0)
+
+		// Test pattern parsed as expected
+		if d.NumDice != tt.numDice {
+			t.Errorf("NumDice value incorrect, expected: %v got: %v.", tt.numDice, d.NumDice)
+		}
+		if d.Sides != tt.sides {
+			t.Errorf("Sides value incorrect, expected: %v got: %v.", tt.sides, d.Sides)
+		}
+
+		// Test the roll value is within expected range
+		roll := d.Roll()
+
+		if roll != tt.expected {
+			t.Errorf("Roll %v result invalid. expected: %v, got: %v.", d, tt.expected, roll)
+		}
+
+		// Test the string representation of the DiceRoll
+		if d.String() != tt.pattern {
+			t.Errorf("Expected %v got %v", tt.pattern, d)
+		}
+	}
+}
+
+var invalidDicePatternTests = []struct {
+	pattern  string
+	expected error
+}{
+	{"", ErrInvalidPattern},
+	{"zzz", ErrInvalidPattern},
+	{"1X6", ErrInvalidPattern},
+	{"2.2d3.3", ErrInvalidPattern},
+	{"1d2", ErrInvalidSides},
+	{"1d101", ErrInvalidSides},
+	{"0d20", ErrInvalidNumDice},
+	{"20d20", ErrInvalidNumDice},
+}
+
+func TestInvalidDiceRollPatterns(t *testing.T) {
+
+	for _, tt := range invalidDicePatternTests {
+
+		_, err := NewDiceRoll(tt.pattern)
+		if err != tt.expected {
+			t.Errorf("expected %v, got %v", tt.expected, err)
+		}
+	}
+}
